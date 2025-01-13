edcdfe27143c31a6aa791d5a832675d4ec5bc026
imsarann <shitisaesthetic@gmail.com>
2025-01-13 23:59:51 +0530

    init

---
+++ frontend/src/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function List({privateKey, publicKey,  coin }) {
+    
+  return (
+    <div className="w-full text-black text-28px border ">
+        <div className='font-bold'>{coin}</div>
+        <div> {publicKey}</div>
+        <div> {privateKey}</div>
+    </div>
+  )
+}
--- frontend/src/Wallet.jsx
+++ frontend/src/Wallet.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
+import Box from './Box';
+import List from './List';
 export default function Wallet() {
     const location = useLocation();
     const { mnemonics } = location.state || {};
     console.log("heyhey", mnemonics)
   return (
-    <div>
-      <div className='w-full bg-gray-900 text-white text-[20px] py-5 px-10 display-inline' type="text">   
-        {
-        mnemonics.split(" ").map((mnemonic, index) =>{ 
-            console.log("aayiram ",mnemonic);
-            return (
-
-                <div  key={index} className='text-white '>{ mnemonic.toUpperCase() } </div> 
-            )
-        
-        })}
-         </div>
+    <div className='bg-gray-950'>
+      <div className='text-white'>Your Mnemonic Seeds</div>
+      {/* <div className='w-full bg-gray-900 text-white text-[20px] py-5 px-10 grid grid-cols-6' type="text">    */}
+        <Box contents={mnemonics}/> 
+        <List publicKey={"akgkjsnakjdsgfnnlkasjdbgbkasjbdgbkasjgaskldjg"} privateKey={"akgkjsnakjdsgfnnlkasjdbgbkasjbdgbkasjgaskldjg"} coin={"SOLANA"}/>
+         {/* </div> */}
+         
     </div>
   )
 }
